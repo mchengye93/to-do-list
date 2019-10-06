b1f7f2c612e74a5b31d448cc9ab4659e76f7c2d5
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-05 18:31:21 -0700

    addToDo  handle input change

--- client/src/components/AddToDoForm.jsx
+++ client/src/components/AddToDoForm.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 
 class AddToDoForm extends Component {
-
+  constructor() {
+      this.state ={
+          toDo: ''
+      }
+      this.handleInputChange = this.handleInputChange.bind(this);
+  }
+  handleInputChange(event){
+      const target = event.target;
+      const value = target.value;
+      const name = target.name;
+      this.setState( {
+          [name]: value
+      })
+      
+  }
   render() {
     return (
     <form>
-
+        <input type="text" name="toDo" value={this.state.toDo}/>
+        <input type="submit" value="Add"/>
     </form>
     );
   }
